155212db6ddf70446c82fbabd1c6e08ff18fdcae
Andor Hofecker <hof.and.or@gmail.com>
2020-05-15 11:41:33 +0200

    Update com ports to RPI

--- test/tests/tests.cpp
+++ test/tests/tests.cpp
@@ -10,17 +10,8 @@ void setup()
     delay(2000); // To establish serial connection to the device
     
     UNITY_BEGIN();
-    RUN_TEST(sample_test1);
-    RUN_TEST(sample_test2);
-    RUN_TEST(sample_test3);
-    RUN_TEST(extended_test1);
-    RUN_TEST(extended_test2);
-    RUN_TEST(extended_test3);
-    RUN_TEST(extended_test4);
-    RUN_TEST(extended_test5);
-    RUN_TEST(extended_test6);
+    RUN_TEST(fixed_tests);
     RUN_TEST(random_tests);
-    RUN_TEST(evaluation_tests);
     UNITY_END();
 }
 
